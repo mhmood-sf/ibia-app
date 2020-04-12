6e4dc38c16b711c2fe335903ca80d34d781ee162
quantomistro <mahmoodsaifi38@gmail.com>
2020-04-13 01:14:15 +0800

    Add boilerplater for a Logger class

---
+++ src/main/java/ibia/app/Logger.java
@@ -0,0 +1,26 @@
+package ibia.app;
+
+/*
+ * TODO:
+ * Set a flag for whether the app is running in development environment or production.
+ * If it's running in development, log everything to the terminal.
+ * If it's running in production, log everything to a proper file.
+ */
+public class Logger {
+
+    public void debug() {
+        
+    }
+
+    public void info() {
+
+    }
+
+    public void warn() {
+
+    }
+
+    public void error() {
+
+    }
+}
