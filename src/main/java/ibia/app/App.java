06dbcea7070de8b2fe2377ea002d0f3c95d9d577
quantomistro <mahmoodsaifi38@gmail.com>
2020-08-17 20:17:39 +0800

    Update App.java to use TemplateEngine

--- src/main/java/ibia/app/App.java
+++ src/main/java/ibia/app/App.java
@@ -5,12 +5,13 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import ibia.app.SceneUtil;
+import ibia.app.templating.TemplateEngine;
 
 // TODO: add proper documentation
 // TODO: get delegate-placeholder.png image
 // TODO: make scenes for committees + delegates too!
-// TODO: add messages to ALL exceptions ! ! !
+// TODO: add messages to ALL exceptions.
+// TODO: see how to use CSS w/ JavaFX to add themes
 
 public class App extends Application {
     /**
@@ -25,7 +26,7 @@ public class App extends Application {
 
     /**
      * Indicates the current scene being displayed.
-     * The value is always an ID or "Home" (for the
+     * The value is always an entity ID or "Home" (for the
      * welcome screen).
      */
     private static String currentLocation;
@@ -65,33 +66,27 @@ public class App extends Application {
         if (currentLocation.equals(location)) return;
 
         if (location.equals("Home")) {
-            Scene home = SceneUtil.loadFXMLScene("Home");
+            Scene home = TemplateEngine.loadHome();
             window.setScene(home);
         } else {
             String entity = location.substring(0, 3);
             switch (entity) {
                 case "CON":
-                    //Conference conference = DbDriver.fetchOne(path[0]);
-                    Scene conferenceScene = Template.loadConference(/*conference*/);
+                    Scene conferenceScene = TemplateEngine.loadConference(location);
                     window.setScene(conferenceScene);
                     break;
                 case "COM":
-                    //Conference conference = DbDriver.fetchOne(path[0]);
-                    //Committee committee = DbDriver.fetchOne(path[1]);
-                    Scene committeeScene = Template.loadCommittee(/*committee, conference*/);
+                    Scene committeeScene = TemplateEngine.loadCommittee(location);
                     window.setScene(committeeScene);
                     break;
                 case "DEL":
-                    //Conference conference = DbDriver.fetchOne(path[0]);
-                    //Committee committee = DbDriver.fetchOne(path[1]);
-                    //Delegate delegate = DbDriver.fetchOne(path[2]);
-                    Scene delegateScene = Template.loadDelegate(/*delegate, committee, conference*/);
+                    Scene delegateScene = TemplateEngine.loadDelegate(location);
                     window.setScene(delegateScene);
                     break;
                 default:
                     throw new Exception(
                         "Invalid location provided: " + location +
-                        "\nLocation must be an ID or \"Home\"!"
+                        "\nLocation must be an entity ID or 'Home'."
                         );
             }
         }
