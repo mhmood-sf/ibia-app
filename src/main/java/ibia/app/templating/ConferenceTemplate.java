65d9186b269fe9a7a37e8b8a8b837f75081ba6c4
quantomistro <mahmoodsaifi38@gmail.com>
2020-10-16 00:49:21 +0800

    Mostly finished conference templating

--- src/main/java/ibia/app/templating/ConferenceTemplate.java
+++ src/main/java/ibia/app/templating/ConferenceTemplate.java
@@ -2,63 +2,111 @@ package ibia.app.templating;
 
 import java.io.IOException;
 import ibia.app.SceneUtil;
+import java.text.SimpleDateFormat;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import ibia.core.Client;
 import ibia.core.DbDriver;
+import ibia.core.entities.Committee;
 import ibia.core.entities.Conference;
-import javafx.scene.Parent;
+import ibia.core.entities.Delegate;
+import ibia.core.utils.Resolution;
+
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class ConferenceTemplate {
-    private String id;
     private Scene template;
-    private Parent root;
     private Conference instance;
 
     public ConferenceTemplate(String id) throws IllegalArgumentException, IOException {
         if (!id.startsWith("CON")) throw new IllegalArgumentException("Invalid ID provided.");
 
-        this.id = id;
         this.template = SceneUtil.loadFXMLScene("ConferenceTemplate");
-        this.root = template.getRoot();
         this.instance = DbDriver.fetchOne(Conference.class, id);
     }
 
     public Scene fill() {
-        setBreadcrumbsNode();
-        setNameNode();
-        setIdNode();
+        fillBreadcrumbsNode();
+        fillNameNode();
+        fillIdNode();
+        fillDetails();
+        fillStatusButton();
 
         return template;
     }
 
-    private HBox getBreadcrumbs() {
-        VBox container = (VBox)template.getRoot();
-        return (HBox)container.getChildren().get(0);
+    private void fillBreadcrumbsNode() {
+        Text crumb = (Text)template.lookup("#conferenceCrumb");
+        crumb.setText(instance.getName());
     }
 
-    private void setBreadcrumbsNode() {
-        getBreadcrumbs();
-        return;
+    private void fillNameNode() {
+        Text name = (Text)template.lookup("#name");
+        name.setText(instance.getName());
     }
 
-    private Text getNameNode() {
-        return null;
+    private void fillIdNode() {
+        Text id = (Text)template.lookup("#id");
+        id.setText(instance.getId());
     }
 
-    private void setNameNode() {
-        getNameNode();
-        return;
+    private void fillDetails() {
+        // Sets OPENED date
+        Text created = (Text)template.lookup("#created");
+        Date date = instance.getCreated();
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+        created.setText(fmt.format(date));
+
+        // Sets status to ONGOING or FINISHED
+        Text status = (Text)template.lookup("#status");
+        String currentStatus = instance.isOngoing() ? "ONGOING" : "FINISHED";
+        status.setText(currentStatus);
+
+        // Sets number of committees
+        Text committees = (Text)template.lookup("#committees");
+        ArrayList<Committee> coms = Client.getConferenceCommittees(instance.getId());
+        int i = coms != null ? coms.size() : 0;
+        committees.setText(Integer.toString(i));
+
+        // Sets number of delegates
+        Text delegates = (Text)template.lookup("#delegates");
+        ArrayList<Delegate> dels = new ArrayList<>();
+        if (coms != null) {
+            for (Committee com : coms) {
+                ArrayList<Delegate> fetched = Client.getCommitteeDelegates(com.getId());
+                if (fetched != null) {
+                    dels.addAll(fetched);
+                }
+            }
+        }
+        delegates.setText(Integer.toString(dels.size()));
+
+        // Sets number of resolutions
+        Text resolutions = (Text)template.lookup("#resolutions");
+        ArrayList<Resolution> resos = DbDriver.findAll(Resolution.class, r -> r.getPassed());
+        int totalResos = 0;
+        if (resos != null) {
+            for (Resolution reso : resos) {
+                for (Delegate del : dels) {
+                    if (del.getId().equals(reso.getMainSubmitter())) {
+                        totalResos += 1;
+                    }
+                }
+            }
+        }
+        resolutions.setText(Integer.toString(totalResos));
     }
 
-    private Text getIdNode() {
-        return null;
-    }
-
-    private void setIdNode() {
-        getIdNode();
-        return;
+    private void fillStatusButton() {
+        Button btn = (Button)template.lookup("#statusButton");
+        if (instance.isOngoing()) {
+            btn.setText("Finish Conference");
+        } else {
+            btn.setText("Re-open Conference");
+        }
     }
 }
