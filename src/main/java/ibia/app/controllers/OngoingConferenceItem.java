1c620ef375b9fb9bf011cf568fbd0aeb50b645c7
quantomistro <mahmoodsaifi38@gmail.com>
2020-10-20 09:36:52 +0800

    ongoing conf item works now

---
+++ src/main/java/ibia/app/controllers/OngoingConferenceItem.java
@@ -0,0 +1,37 @@
+package ibia.app.controllers;
+
+import java.io.IOException;
+
+import ibia.app.App;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+
+public class OngoingConferenceItem {
+    @FXML protected HBox container;
+    @FXML protected Text id;
+
+    @FXML
+    protected void hoverItemEffectOn(MouseEvent event) {
+        BackgroundFill bgFill = new BackgroundFill(Paint.valueOf("#363648"), null, null);
+        Background bg = new Background(bgFill);
+        container.setBackground(bg);
+    }
+
+    @FXML
+    protected void hoverItemEffectOff(MouseEvent event) {
+        BackgroundFill bgFill = new BackgroundFill(null, null, null);
+        Background bg = new Background(bgFill);
+        container.setBackground(bg);
+    }
+
+    @FXML
+    protected void navigate(MouseEvent event) throws IOException, IllegalArgumentException {
+        String confId = id.getText();
+        App.navigate(confId);
+    }
+}
