13af8e7aea7d0f366d1fae4a437827d0cd692252
quantomistro <mahmoodsaifi38@gmail.com>
2020-10-21 22:26:58 +0800

    Implement NewDelegate popup

---
+++ src/main/java/ibia/app/controllers/NewDelegate.java
@@ -0,0 +1,77 @@
+package ibia.app.controllers;
+
+import java.util.ArrayList;
+
+import ibia.app.App;
+import ibia.app.SceneUtil;
+import ibia.core.Client;
+import ibia.core.Log;
+import ibia.core.entities.Delegate;
+import ibia.core.utils.Country;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class NewDelegate {
+    @FXML protected TextField name;
+    @FXML protected TextField delegation;
+    @FXML protected Button cancel;
+    @FXML protected Button confirm;
+    @FXML protected MenuButton choose;
+
+    @FXML
+    public void initialize() {
+        ArrayList<String> countries = Country.listOfNames();
+
+        for (String country : countries) {
+            MenuItem choice = new MenuItem();
+            choice.setText(country);
+            choice.setOnAction((ActionEvent event) -> {
+                delegation.setText(country);
+            });
+        }
+    }
+
+    @FXML
+    protected void create(MouseEvent event) {
+        String delName = name.getText();
+        // validate form data
+        if (delName.isEmpty()) {
+            SceneUtil.error("The delegate name is required!").show();
+        }
+        else if (delName.length() > 120) {
+            SceneUtil.error("The delegate name must be between 1 and 120 characters!").show();
+        }
+        else {
+            try {
+                String comId = App.getLocation();
+                String delegationName = delegation.getText();
+                Delegate del = Client.addNewDelegate(delName, delegationName, comId);
+                App.navigate(del.getId());
+                closeStage(event);
+            } catch (Exception e) {
+                Log.error(e.getMessage());
+                e.printStackTrace();
+                SceneUtil.error(e.getMessage()).show();
+            }
+        }
+    }
+
+    @FXML
+    protected void cancel(MouseEvent event) {
+        closeStage(event);
+    }
+
+    private void closeStage(MouseEvent event) {
+        // cast source to Button so we can access window
+        Button source = (Button)(event.getSource());
+        // get window, cast it to Stage so we can close it
+        Stage stage = (Stage)(source.getScene().getWindow());
+        stage.close();
+    }
+}
